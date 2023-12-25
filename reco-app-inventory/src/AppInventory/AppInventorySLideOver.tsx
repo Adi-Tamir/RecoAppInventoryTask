3674a372f4a2f75ea7c168da37fc7c35033b30ca
Adi-Tamir <4adidi@gmail.com>
2023-12-25 12:57:48 +0200

    slide over definition

--- reco-app-inventory/src/AppInventory/AppInventorySLideOver.tsx
+++ reco-app-inventory/src/AppInventory/AppInventorySLideOver.tsx
@@ -1,7 +1,26 @@
+import { Text } from "../Components/Text";
+
 export function AppInventorySlideOver({app}:{app:any}) {
     return (
-     <>Slide Over</>
+     
+     <div>
+        <Text>App overview</Text>
+        <AppOverview appId={app.appId}/>
+        <AppUsers appId={app.appId} />
+     </div>
     );
+  }
+
+  function AppOverview({appId}: {appId:string}){
+    return (<div>
+
+    </div>);
+  }
+
+  function AppUsers({appId}: {appId: string}){
+    return (<div>
+        
+    </div>);
   }
   
  
